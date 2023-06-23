dc9972bda75cba7ccbaee0a79fa822e671517c02
mczupryn1 <81737272+mczupryn1@users.noreply.github.com>
2023-06-23 11:59:29 -0400

    completed app

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const apiRoutes = require('./routes/api-routes');
-const htmlRoutes = require('./routes/html-routes');
-const exphbs = require('express-handlebars');
+const express = require('express')
+const apiRoutes = require('./routes/api-routes')
+const htmlRoutes = require('./routes/html-routes')
+const app = express()
 
-const app = express();
+const exphbs = require('express-handlebars')
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-// Correct usage of express-handlebars
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-app.use('/', htmlRoutes);
-app.use('/api', apiRoutes);
+app.use('/', htmlRoutes)
+app.use('/api', apiRoutes)
 
-module.exports = app;
+module.exports = app
